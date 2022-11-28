9eb6f212d608d338903b849bcf1ef23e4d17a064
aowuor <angelaowuor@gmail.com>
2022-11-28 23:08:34 +0300

    commit tree 53496eb1f5c243d082f7f4cf18aa1fe29268fab7

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(arr, driver){
+    const matchingDriver = arr.filter(name => name.toUpperCase() === driver.toUpperCase());
+    return matchingDriver;
+}
+console.log(findMatching(drivers, 'Bobby'));
+
+
+function fuzzyMatch(arr, char){
+    const beginningSame = arr.filter(name => name.startsWith(char));
+    return beginningSame;
+}
+console.log(fuzzyMatch(drivers, 'Sa'));
+
+const drivers2 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(arr, name){
+    const matchingName = arr.filter(driver => driver.name === name);
+    return matchingName;
+
+}
+console.log(matchName(drivers2, 'Bobby'))
